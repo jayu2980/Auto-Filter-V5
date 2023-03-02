d1e9253a3f87ef200c1cfcc44218d6131eb80463
jayu2980 <124619960+jayu2980@users.noreply.github.com>
2023-03-03 00:50:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ SPL_DELETE_TIME = int(environ.get('SPL_DELETE_TIME', 15))
 URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'mdisklink.online')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '5235bb79c2e5d6700121867eb46761c28b98d31e')
 
-LOG_STR = "Current Cusomized Configurations are:-\n"
+LOG_STR = "Current Cusomized Configurations are:-\n")
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
 LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
 LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
